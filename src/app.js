9e6ddce8d5e11d6f00a6fac22d2c723785a7d788
Roman Perepelkin <perepelkinrs@gmail.com>
2024-11-19 14:29:37 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+function copyFile() {
+  const [from, to] = process.argv.slice(2);
+
+  if (from === to) {
+    console.error('error');
+
+    return;
+  }
+
+  if (!from || !to) {
+    return console.error('error');
+  }
+
+  fs.cp(from, to, (error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
+}
+
+copyFile();
